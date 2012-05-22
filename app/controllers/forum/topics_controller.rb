c7efeaaafa19b191df3ec4d2a500870449697a67
kosteven <chunyuko85@gmail.com>
2012-05-22 15:39:40 +0800

    forum detail 頁
    Signed-off-by: kosteven <chunyuko85@gmail.com>

--- app/controllers/forum/topics_controller.rb
+++ app/controllers/forum/topics_controller.rb
@@ -1,4 +1,4 @@
-class TopicsController < ApplicationController
+class Forum::TopicsController < ApplicationController
   before_filter :authenticate_user! ,:only => [ :new , :create , :edit , :update , :destroy ]
   
   def new
