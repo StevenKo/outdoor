e41c691435629aa739c8bd22c798c5a7b8464153
kosteven <chunyuko85@gmail.com>
2012-06-26 16:47:33 +0800

    修正 helper 錯誤
    Signed-off-by: kosteven <chunyuko85@gmail.com>

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,9 @@
 # encoding: utf-8
 module PostsHelper
   def render_collect_btn post
+    
+    return button_to "收藏",add_collect_post_path(@post), :class => "btn btn-success", :remote => true, :method => :put, :id => "btn-collect" unless current_user
+    
     if current_user.collect(post)
       button_to "已收藏",add_collect_post_path(@post), :class => "btn btn-danger", :remote => true, :method => :put, :id => "btn-collect"
     else  
@@ -8,6 +11,9 @@ module PostsHelper
     end
   end
   def render_follow_btn user
+    
+    return button_to "關注",follow_user_path(user), :class => "btn btn-info", :remote => true, :method => :put, :id => "btn-follow" unless current_user
+    
     if current_user.follow(user)
       button_to "已關注",follow_user_path(user), :class => "btn btn-info", :remote => true, :method => :put, :id => "btn-follow"
     else  
